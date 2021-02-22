ab0aecc5d52437f40773683bef8576f7bb3884a5
Riley Grotenhuis <rileygrotenhuis@Rileys-MBP-010011065195.MissouriState.edu>
2021-02-22 14:42:47 -0600

    Create routes to a simple home, login, and register page with a simple navigation bar.

--- server.js
+++ server.js
@@ -2,6 +2,24 @@
 const express = require('express');
 const app = express();
 
+// View Engine
+app.set('view engine', 'ejs');
+
+// GET Home Page => '/'
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+// GET Login Page => '/login'
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+// GET Register Page => '/register'
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
 // Listen
 app.listen(3000, () => {
     console.log('Bear Forum is running at http://localhost:3000');
