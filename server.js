c0ec8a5ff6a9eaad20470f9e2f681b44f67f1aa8
Riley Grotenhuis <rileygrotenhuis@Rileys-MBP-010011065195.MissouriState.edu>
2021-02-22 17:22:02 -0600

    Add registration functionality.

--- server.js
+++ server.js
@@ -55,15 +55,34 @@ app.delete('/logout', (req, res) => {
 
 // GET Register Page => '/register'
 app.get('/register', (req, res) => {
-    res.render('register');
+    res.render('register', { flash: req.flash('msg') });
 });
+
+// POST Register Page => '/register'
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+    try {
+        // Query through the db for a User with the Username given in the Registration Form
+        const checkUser = await pool.query("SELECT * FROM users WHERE username = $1", [req.body.username]);
+        // If the User does not already exist, add a new User to the db
+        if (checkUser.rows[0] == null) {
+            const data = await pool.query("INSERT INTO users VALUES ($1, $2, $3, $4, $5, $6, $7, $8)", [req.body.username, req.body.password, req.body.first_name, req.body.last_name, req.body.user_email, req.body.location_city, req.body.location_state, req.body.biography]);
+            res.redirect('/login');
+        // Otherwise, display an error message and redirect to the register page
+        } else {
+            req.flash('msg', 'Username already exists');
+            res.redirect('/register');
+        }
+    } catch (e) {
+        console.error(e);
+        res.redirect('/register');
+    }
+})
 
 // Authenticated Check
 function checkAuthenticated (req, res, next) {
     if (req.isAuthenticated()) {
         return next();
     }
-
     res.redirect('/login');
 }
 
