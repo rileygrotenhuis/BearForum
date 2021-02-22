c0fd8a08a1619d6ff548ce283a244b7394a558e3
Riley Grotenhuis <rileygrotenhuis@Rileys-MBP-010011065195.MissouriState.edu>
2021-02-21 19:30:57 -0600

    Create simple ExpressJS application.

---
+++ server.js
@@ -0,0 +1,8 @@
+// Node Packages
+const express = require('express');
+const app = express();
+
+// Listen
+app.listen(3000, () => {
+    console.log('Bear Forum is running at http://localhost:3000');
+});
