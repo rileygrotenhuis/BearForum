e68fdfd763058fb2806a41dd5fd43b2b1a0e69ea
Riley Grotenhuis <rileygrotenhuis@Rileys-MBP-010011065195.MissouriState.edu>
2021-02-25 22:59:07 -0600

    Users are now able to update one of their posts if they are logged in.

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -22,4 +22,31 @@ router.get('/:userID/:postID', async (req, res) => {
     }
 });
 
+// GET '/:userID/:postID/update' => Render the Update Post Form
+router.get('/:userID/:postID/update', checkAuthenticated, async (req, res) => {
+    try {
+        // Query through the database and get the selected post
+        const post = await pool.query("SELECT * FROM posts WHERE post_id = $1", [req.params.postID]);
+        res.render('../views/post/update', {
+            user: req.user,
+            post: post.rows[0]
+        });
+    } catch (e) {
+        console.error(e);
+        res.redirect('/' + req.params.userID);
+    }
+});
+
+// PUT '/:userID/:postID/update' => Update the post in the database with the new information
+router.put('/:userID/:postID/update', checkAuthenticated, async (req, res) => {
+    try {
+        await pool.query("UPDATE posts SET title = $1, topic = $2, content = $3 WHERE post_id = $4", [req.body.title, req.body.topic, req.body.content, req.params.postID]);
+
+        res.redirect('/' + req.params.userID + '/' + req.params.postID);
+    } catch (e) {
+        console.error(e);
+        res.redirect('/' + req.params.userID + '/' + req.params.postID);
+    }
+});
+
 module.exports = router;
